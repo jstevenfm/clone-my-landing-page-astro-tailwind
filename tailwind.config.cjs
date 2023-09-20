46cc0c04e5cee35600721777ed28c1b72501b256
jstevenfm <jstevenfm@gmail.com>
2023-09-20 16:53:12 -0500

    Add - Added main Layout with Header and main custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'royalblue': '#27336E',
+				'pinkpurple': '#8478F8',
+				'ctagreen': '#73FBD3'
+			},
+			fontFamily: {
+				'notosans': ['"Noto Sans JP"', 'sans-serif;']
+			},
+		},
 	},
 	plugins: [],
 }
